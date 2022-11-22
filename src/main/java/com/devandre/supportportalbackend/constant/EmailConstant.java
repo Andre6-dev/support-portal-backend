135df5d449c25cd82e9293e9301d76e27a70c72c
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2022-11-22 13:03:11 -0500

    Fix email constants.

--- src/main/java/com/devandre/supportportalbackend/constant/EmailConstant.java
+++ src/main/java/com/devandre/supportportalbackend/constant/EmailConstant.java
@@ -7,8 +7,8 @@ package com.devandre.supportportalbackend.constant;
 public class EmailConstant {
 
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "im.andre6dev@gmail.com";
-    public static final String PASSWORD = "zhoxizosirovgwvy";
+    public static final String USERNAME = "email";
+    public static final String PASSWORD = "password";
     public static final String FROM_EMAIL = "support@devandre.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "User Management Project, DevAndre - New Password";
